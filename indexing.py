79e6741ce54f6357bcb235d7f7cc43e5e59d7576
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2021-12-04 02:29:40 +0100

    fetch def

--- indexing.py
+++ indexing.py
@@ -20,7 +20,8 @@ def parse_folder(folder_path, parent_element):
                           name=file,
                           type=splitext(file)[-1][1:].upper(),
                           size=str(getsize(join(folder_path, file))),
-                          modified=str(time.ctime(getmtime(join(folder_path, file))))
+                          modified=str(time.ctime(getmtime(join(folder_path, file)))),
+                          full_path=str(join(folder_path, file))
                           ).text = canny_detection.get_canny_from_img(join(folder_path, file))
 
             file_count += 1
@@ -40,17 +41,17 @@ def parse_folder(folder_path, parent_element):
 
 def create_master_data_xml(path):
     try:
-        print("(i) Master data file creation started")
+        print("\n>> Master data file creation started")
         root = ET.Element("root_directory", path=path)
 
         parse_folder(path, root)
 
         tree = ET.ElementTree(root)
         tree.write("master_data.xml", encoding='utf-8', xml_declaration=True, pretty_print=True)
-        print("(i) Successfully created master data file")
+        print("    (i) Successfully created master data file")
 
     except:
-        print("(!) Failed to create master data file")
+        print("    (!) Failed to create master data file")
 
 
 def get_master_data_file_count(file_type=""):
@@ -71,6 +72,67 @@ def get_master_data_file_count(file_type=""):
     return count
 
 
+def fetch_changes():
+    print("\n>> Fetching changes...", end="")
+
+    # Open master file
+    with open("master_data.xml") as master_file:
+        data = ET.parse(master_file)
+        removed_files = 0
+        removed_folders = 0
+        removed_files_through_folder = 0
+
+        # Parse through every element
+        for elem in data.iter():
+
+            # Folder element
+            if elem.tag == "folder":
+                # Delete xml folder element if folder not found in path
+                if not os.path.exists(elem.get("full_path")):
+                    parent = elem.getparent()
+                    parent.remove(elem)
+                    removed_files_through_folder += int(elem.get("file_count"))
+                    removed_folders += 1
+
+            # File element
+            elif elem.tag == "file":
+                # Delete xml file element if file not found in path
+                if not os.path.exists(elem.get("full_path")):
+                    parent = elem.getparent()
+                    parent.remove(elem)
+                    removed_files += 1
+                # Delete xml file element if it has changed the properties size or modified date
+                else:
+                    if elem.get("size") != str(getsize(elem.get("full_path"))) or \
+                            elem.get("modified") != str(time.ctime(getmtime(elem.get("full_path")))):
+                        parent = elem.getparent()
+                        parent.remove(elem)
+                        removed_files += 1
+
+        # Save new master file
+        data.write("master_data.xml", encoding='utf-8', xml_declaration=True, pretty_print=True)
+
+    # Print results
+    print("finished.")
+    if removed_files == 1:
+        print("    1 file removed")
+    else:
+        print("    " + str(removed_files) + " files removed")
+
+    if removed_folders == 0:
+        print("    0 folders removed")
+    else:
+        if removed_folders == 1:
+            print("    1 folder removed, ", end="")
+        else:
+            print("    " + str(removed_folders) + " folders removed, ", end="")
+
+        if removed_files_through_folder == 1:
+            print("including 1 file")
+        else:
+            print("including " + str(removed_files_through_folder) + " files")
+
+
 if __name__ == "__main__":
-    # create_master_data_xml(r"E:\=Eigene Dateien=\Bilder\Verschiedene Bilder\Whats App\Bilder")
-    print(get_master_data_file_count("jpeg"))
+    # create_master_data_xml(r"C:\Users\patri\Documents\GitHub\FileManager\test_data")
+    fetch_changes()
